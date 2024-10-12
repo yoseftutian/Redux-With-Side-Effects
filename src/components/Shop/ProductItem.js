6c2458217c7e8f32ea914a7135181282be117562
yosef-tutian <yoseftutian@gmail.com>
2024-10-12 20:33:22 +0300

    add DUMMY_PRODUCTS and option to add or remoove item from cart

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,9 +1,15 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
 
+  const addToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({ id, title, price }));
+  };
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +19,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
