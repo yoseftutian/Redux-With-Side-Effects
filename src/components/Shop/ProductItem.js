ddd0c98251033e30ae9a6c703e4b9c95b8a02d8b
yosef-tutian <yoseftutian@gmail.com>
2024-10-14 16:50:40 +0300

    connect redux to DB with useEffect

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,15 +1,27 @@
 import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
-import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({ id, title, price }));
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
+
   return (
     <li className={classes.item}>
       <Card>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,12 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -38,4 +42,5 @@ const cartSlice = createSlice({
 });
 
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
